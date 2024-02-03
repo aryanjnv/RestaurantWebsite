2011795560f2a50c4da9eff13336b30146413a21
aryanjnv <aryangpjnv188@gmail.com>
2024-02-03 19:43:02 +0530

    Add Restaurant Summary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './components/Layout/Header';
 import { Fragment } from 'react';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <Fragment>
      <Header/>
+     <main>
+      <Meals/>
+     </main>
     </Fragment>
   );
 }
